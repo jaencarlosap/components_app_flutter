46e3ec3fef17f255be4b421ec34c05ecc09ecd48
japonte254 <jeancarloss659@gmail.com>
2021-02-20 19:41:19 -0500

    - create first app with components - create new alert and avatar page

---
+++ lib/src/pages/home_temp.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class HomePageTemp extends StatelessWidget {
+  final opciones = ['uno', 'dos', 'tres', 'cuatro', 'cinco'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes Temp'),
+      ),
+      body: ListView(
+        children: _crearItemsCorta(),
+      ),
+    );
+  }
+
+  List<Widget> _crearItems() {
+    List<Widget> lista = new List<Widget>();
+
+    for (String opt in opciones) {
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+
+      lista..add(tempWidget)..add(Divider());
+    }
+
+    return lista;
+  }
+
+  List<Widget> _crearItemsCorta() {
+    return opciones.map((item) {
+      return Column(
+        children: [
+          ListTile(
+            title: Text(item),
+            subtitle: Text('algo'),
+            leading: Icon(Icons.ac_unit),
+            trailing: Icon(Icons.access_alarm_sharp),
+            onTap: () {},
+          ),
+          Divider(),
+        ],
+      );
+    }).toList();
+  }
+}
