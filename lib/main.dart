a6eee17d8dbbe89ae6043edd7e494f478f6982f5
unknown <jeancarloss659@gmail.com>
2021-03-27 17:07:39 -0500

    create new page of the inputs

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/card_pages.dart';
 import 'package:componentes/src/pages/animated_container.dart';
+import 'package:componentes/src/pages/input_page.dart';
 // import 'package:componentes/src/pages/home_temp.dart';
 
 void main() => runApp(MyApp());
@@ -22,6 +23,7 @@ class MyApp extends StatelessWidget {
         'avatar': (BuildContext context) => AvatarPage(),
         'card': (BuildContext context) => CardPage(),
         'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
+        'inputs': (BuildContext context) => InputPage(),
       },
     );
   }
---
+++ lib/src/pages/input_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  String _nombre = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Title'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+        children: [
+          _crearInput(),
+          Divider(),
+          _imprimirPersona(),
+        ],
+      ),
+    );
+  }
+
+  Widget _crearInput() {
+    return TextField(
+      autofocus: true,
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+        counter: Text('Letras ${_nombre.length}'),
+        hintText: 'Nombre de persona',
+        labelText: 'Nombre',
+        suffixIcon: Icon(Icons.accessibility),
+        icon: Icon(Icons.account_circle),
+      ),
+      onChanged: (value) {
+        setState(() {
+          _nombre = value;
+        });
+      },
+    );
+  }
+
+  Widget _imprimirPersona() {
+    return ListTile(
+      title: Text('Nombre: $_nombre'),
+    );
+  }
+}
